d3fc7b9ffca7aeada68c11e8ac9f7f481cfb3cc8
Max <maxclifordjean@gmail.Com>
2018-01-08 09:53:38 +0100

    initial app ruby

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'http://rubygems.org'
+
+gem 'sinatra'
+gem 'em-websocket'
+gem 'thin'
+gem 'json'
---
+++ app.rb
@@ -0,0 +1,45 @@
+require 'rubygems'
+require 'sinatra'
+require 'em-websocket'
+require 'thin'
+require 'json'
+
+EM.run do
+  class App < Sinatra::Base
+    before do
+      headers 'Access-Control-Allow-Origin' => '*', 'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST']
+    end
+
+    enable :sessions
+    #register Sinatra::Flash
+    set :protection, false
+    #set :views, settings.root + "/views"
+    #set :root, 'lib/app'
+
+    #start websocket server including EventMachine
+    EM::WebSocket.start(:host => '0.0.0.0', :port => '4247') do |ws| #we fixe our websocket connection and instanciate it with "ws"
+      ws.onopen do |handshake|  #Event=open, occurs when socket connection is established
+        puts "WS OPEN"
+      end
+
+      ws.onclose do   #Event=close, occurs when socket connection is closed
+        puts "WS CLOSED"
+        ws.send "Closed."
+      end
+
+      ws.onmessage do |msg| #Event=message, occurs when client receives data from server
+        puts "message sending...: #{ws}"
+        ws.send msg
+      end
+    end
+
+    get '/' do
+      render :html, :index
+    end
+
+
+  end
+
+  # run! if __FILE__ == $0
+  App.run! :port => 4246, :bind => '0.0.0.0'
+end
